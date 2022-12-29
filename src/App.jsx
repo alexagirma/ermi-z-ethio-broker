0a04436f1970c1aad28938f08fe1cea3b89f2125
alexagirma <alexagirma255@gmail.com>
2022-12-29 11:31:14 +0300

    frontend part

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import MasterLayout from "./layouts/admin/MasterLayout";
 import Dashboard from "./components/admin/Dashboard";
 import Profile from "./components/admin/Profile";
+import Home from "./components/frontend/Home"
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
+          <Route path="/" element={<Home />}></Route>
           <Route path="/admin" element={<MasterLayout />}></Route>
           <Route path="/admin/dashboard" element={<Dashboard />}></Route>
           <Route path="/admin/profile" element={<Profile />}></Route>
---
+++ src/components/frontend/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Home() {
+    return (
+        <div>
+            <h1>Home</h1>
+            <p>
+                This is a home component.
+                <br />
+                It can be used to display a home theme in a home page.
+            </p>
+            </div>
+    )
+}
+
+export default Home;
